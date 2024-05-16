07121aaca063b533a52cd2d6044e32beca9131e2
bakk0u <anasbakkoury@gmail.com>
2024-05-17 00:08:26 +0200

    Update version2.py

---
+++ version2.py
@@ -0,0 +1,36 @@
+# combining the two functions inside the caeser() function and polish more our previous code 
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+            'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+              'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == 'decode':
+            shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+             end_text += char
+             continue
+    print(f"The {cipher_direction}d text is {end_text}")
+    
+if shift > 52:
+     shift %= 26
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+while restart == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 52:
+         shift %= 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+print("Goodbye!")
